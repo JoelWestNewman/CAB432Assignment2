da7e721acfa6491a46314c1d437bd63c189a2d2b
Joel WestNewman <joelwestnewman@gmail.com>
2021-10-16 13:42:17 +1000

    base react app with node express backend created

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+<h1> machine learning application</h1>
+
+
+
+    </div>
+  );
+}
+
+export default App;
