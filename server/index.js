1e6705a1ea3ab1cb79c7098dc8a1fac50aad70ed
CostaAspro708 <n10464174@qut.edu.au>
2021-10-27 01:27:11 +1000

    /predict endpoint

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+
 const PORT = process.env.PORT || 3001;
 const express = require("express");
 const responseTime = require("response-time");
@@ -47,7 +48,7 @@ function readImage(path){
 function imageByteArray(image, numChannels){
   const pixels = image.data
   const numPixels = image.width * image.height;
-  const values = new Int32Array(numPixels * numChannels);s
+  const values = new Int32Array(numPixels * numChannels);
   for (let i = 0; i < numPixels; i++) {
     for (let channel = 0; channel < numChannels; ++channel) {
       values[i * numChannels + channel] = pixels[i * 4 + channel];
